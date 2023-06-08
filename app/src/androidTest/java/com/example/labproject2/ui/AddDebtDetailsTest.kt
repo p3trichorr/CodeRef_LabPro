7f5a8b98ecab1f5cd969bc45cda2814a26c4c3ed
Zulfiqar Rahman Aji <zulfiqar.aji@gmail.com>
2023-06-08 13:04:31 +0700

    minor update

---
+++ app/src/androidTest/java/com/example/labproject2/ui/AddDebtDetailsTest.kt
@@ -0,0 +1,42 @@
+//import androidx.appcompat.widget.AppCompatButton
+//import org.junit.Assert.assertEquals
+//import org.junit.Before
+//import org.junit.Test
+//import org.mockito.Mock
+//import org.mockito.Mockito.*
+//import org.mockito.MockitoAnnotations
+
+class AddDebtDetailsTest {
+
+//    @Mock
+//    private lateinit var mockBinding: AddDebtDetailsBinding
+//
+//    @Mock
+//    private lateinit var mockPresenter: DebtPresenter
+//
+//    private lateinit var addDebtDetails: AddDebtDetails
+//
+//    @Before
+//    fun setUp() {
+//        MockitoAnnotations.initMocks(this)
+//        // Create an instance of the class to be tested
+//        addDebtDetails = AddDebtDetails()
+//        addDebtDetails.binding = mockBinding
+//        // Set up any necessary dependencies or mocks
+//        addDebtDetails.presenter = mockPresenter
+//    }
+//
+//    @Test
+//    fun `test save button click listener`() {
+//        // Set up
+//        val mockSaveBtn = mock(AppCompatButton::class.java)
+//        `when`(mockBinding.saveBtn).thenReturn(mockSaveBtn)
+//        // Call the click listener manually or simulate button click event
+//        addDebtDetails.initializeSaveBtnClickListener()
+//
+//        // Assert the expected behavior
+//        verify(mockSaveBtn).setOnClickListener(any())
+//
+//        // TODO: Continue with assertions for the expected behavior resulting from the click event
+//    }
+}
